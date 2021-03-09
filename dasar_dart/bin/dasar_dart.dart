e9f57211bf05eea9ca666c0e69ac38f69b011f43
AbidahNabilah <abdhnabilah@gmail.com>
2021-03-09 10:18:57 +0700

    minggu1

--- dasar_dart/bin/dasar_dart.dart
+++ dasar_dart/bin/dasar_dart.dart
@@ -1,5 +1,23 @@
-import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dasar_dart.calculate()}!');
+
+import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
+import 'dart:io';
+
+// void main(List<String> arguments) {
+//   String input = stdin.readLineSync();
+//   double number = double.tryParse(input);
+//   print (number + 10);
+// }
+
+main(List<String> args) {
+  int number = int.tryParse(stdin.readLineSync());
+  String output;
+  if (number > 0){
+    print("positif");
+  else if (number < 0 ){
+    print ("negatif");
+  }else{
+    print("nol");
+  }
 }
+
