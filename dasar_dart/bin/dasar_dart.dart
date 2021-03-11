72ec73a08a29627c7f23be63207faff4f46baa0c
AbidahNabilah <abdhnabilah@gmail.com>
2021-03-11 21:56:21 +0700

    1

--- dasar_dart/bin/dasar_dart.dart
+++ dasar_dart/bin/dasar_dart.dart
@@ -3,21 +3,10 @@
 import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
 import 'dart:io';
 
-// void main(List<String> arguments) {
-//   String input = stdin.readLineSync();
-//   double number = double.tryParse(input);
-//   print (number + 10);
-// }
+void main(List<String> arguments) {
+  String input = stdin.readLineSync();
+  double number = double.tryParse(input);
+  print (number + 10);
+ }
 
-main(List<String> args) {
-  int number = int.tryParse(stdin.readLineSync());
-  String output;
-  if (number > 0){
-    print("positif");
-  else if (number < 0 ){
-    print ("negatif");
-  }else{
-    print("nol");
-  }
-}
 
---
+++ dasar_dart/bin/if.dart
@@ -0,0 +1,15 @@
+import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
+import 'dart:io';
+
+main(List<String> args) {
+  int number =int.tryParse(stdin.readLineSync());
+  String output;
+
+  // output = (number > 0) ? "positif" : "negatif atau nol";
+  // if (number > 0){
+  //  output = "positif";
+  // } else {
+  //  output = "negatif atau nol";
+  // }
+  print ((number > 0) ? number : number * -1);
+}
